9d6f340a40392f1db4fe9f7c37a3b3519242b109
Ivan <uzvano@mail.ru>
2024-07-01 20:31:58 +0300

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 34945;
+        int ruble = 20;
+
+        System.out.println("Количество бонусных миль: " + price / ruble);
+    }
+}
